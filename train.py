a1ab8beefa2a15d609aabc32266be5b7048038d9
catLee <1204399921@qq.com>
2024-05-13 22:58:58 +0800

    init

--- train.py
+++ train.py
@@ -513,9 +513,9 @@ def train(hyp, opt, device, callbacks):
 def parse_opt(known=False):
     """Parses command-line arguments for YOLOv5 training, validation, and testing."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--weights", type=str, default=ROOT / "yolov5s.pt", help="initial weights path")
-    parser.add_argument("--cfg", type=str, default="", help="model.yaml path")
-    parser.add_argument("--data", type=str, default=ROOT / "data/coco128.yaml", help="dataset.yaml path")
+    parser.add_argument("--weights", type=str, default=ROOT / "weight/yolov5s.pt", help="initial weights path")
+    parser.add_argument("--cfg", type=str, default=ROOT / "models/yolov5s_fire.yaml", help="model.yaml path")
+    parser.add_argument("--data", type=str, default=ROOT / "data/fire.yaml", help="dataset.yaml path")
     parser.add_argument("--hyp", type=str, default=ROOT / "data/hyps/hyp.scratch-low.yaml", help="hyperparameters path")
     parser.add_argument("--epochs", type=int, default=100, help="total training epochs")
     parser.add_argument("--batch-size", type=int, default=16, help="total batch size for all GPUs, -1 for autobatch")
